b3230d1d05e2c4e45701422e97b1e03f7500db18
Serhii Panasiuk <regidmail.sergej@gmail.com>
2021-10-28 00:50:53 +0300

    upd exercise 1, exercise 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,60 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-console.log('Первая домашка');
+// console.log('Первая домашка');
+
+const refer = {
+  containerGallery: document.querySelector('.gallery'),
+};
+
+createGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  close: false,
+  captionDelay: 250,
+  captionType: 'data',
+});
+
+refer.containerGallery.addEventListener('click', e => {
+  if (e.target === e.currentTarget) return;
+
+  document.body.style.paddingRight = 0;
+
+  const originalImage = document.querySelector('.simple-lightbox');
+
+  const onCloseOriginalImage = e => {
+    if (e.target.localName === 'img') {
+      lightbox.close();
+    }
+  };
+
+  originalImage.addEventListener('click', onCloseOriginalImage);
+
+  lightbox.on('close.simplelightbox', function () {
+    originalImage.removeEventListener('click', onCloseOriginalImage);
+  });
+});
+
+function createGallery(galleryItems) {
+  refer.containerGallery.insertAdjacentHTML(
+    'beforeend',
+    galleryItems
+      .map(galleryItem => {
+        return createPicture(galleryItem);
+      })
+      .join(''),
+  );
+}
+
+function createPicture({ preview, original, description }) {
+  return `  
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-title="${description}" />
+  </a>
+  `;
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import * as storage from './storage';
 
+// console.log(storage);
+
+const startPlaybackRecord = storage.load('videoplayer-current-time');
+
+const storageTimeRecording = e => {
+  //   console.log(e.seconds);
+  //   console.log(`Запись в хранилище`);
+  storage.save('videoplayer-current-time', e.seconds);
+};
+
+var idPlayer = new player('vimeo-player');
+
+if (startPlaybackRecord) {
+  idPlayer
+    .setCurrentTime(startPlaybackRecord)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+
+idPlayer.on('timeupdate', throttle(storageTimeRecording, 1000));
+
+// console.log(throttle);
+// console.log(idPlayer);
